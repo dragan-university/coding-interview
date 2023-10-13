16330be427c754aa88acd7840e4d7802c5f2e7c9
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2023-10-14 01:57:01 +0200

    minor update

--- leetcode/q0001-two-sum/solution2.py
+++ leetcode/q0001-two-sum/solution2.py
@@ -22,6 +22,11 @@ class Solution:
         raise Exception('No solution for twoSum!')
     
     def findIndex(self, nums: List[int], element: int, count: int) -> int:
+        """
+        Find 'count' occurence of the 'element' in the list 'nums'
+
+        Example: findIndex([1,7,4,7,9,1], 7, 2) returns 3
+        """
         i = 0
         c = 1
         
